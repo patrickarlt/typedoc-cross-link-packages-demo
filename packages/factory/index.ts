38557161d6274cc5f4d042280c6c911e060ef532
Patrick Arlt <patrick.arlt@gmail.com>
2021-06-24 11:10:48 -0700

    udpate doc

--- packages/factory/index.ts
+++ packages/factory/index.ts
@@ -1,7 +1,5 @@
 import { IceCream, Flavors } from "../shared";
 
-export { IceCream, Flavors } from "../shared";
-
 export function make(flavor: Flavors): IceCream {
   return {
     flavor: flavor,
