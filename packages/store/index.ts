b11fc7baf3af40379cd5d83b0cfbb88aadedc002
Patrick Arlt <patrick.arlt@gmail.com>
2021-06-24 11:12:28 -0700

    doc

--- packages/store/index.ts
+++ packages/store/index.ts
@@ -1,5 +1,7 @@
 import { IceCream, Flavors } from "../shared";
 
+export { IceCream, Flavors } from "../shared";
+
 export function serve(flavor: Flavors): IceCream {
   return {
     flavor: flavor,
